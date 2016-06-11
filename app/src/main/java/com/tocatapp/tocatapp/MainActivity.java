7c66183401419a82ae8a3b91d0e13aac125bfb7d
Augusto Toledo <atoledo@pcorp.cl>
2016-06-10 21:48:25 -0400

    Creates musician registration part 1 at 90%

--- app/src/main/java/com/tocatapp/tocatapp/MainActivity.java
+++ app/src/main/java/com/tocatapp/tocatapp/MainActivity.java
@@ -1,13 +1,20 @@
 package com.tocatapp.tocatapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToRegistration(View view) {
+        Intent intent = new Intent(this, MusicianRegistrationOne.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/tocatapp/tocatapp/MusicianRegistrationOne.java
@@ -0,0 +1,13 @@
+package com.tocatapp.tocatapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MusicianRegistrationOne extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_musician_registration_one);
+    }
+}
